08d3f1e729d4e53e1c3a3bcbf4e84e737a607d7d
john <john@mail.aifu.com.tw>
2023-10-20 18:43:50 +0800

    初始化

---
+++ Core6_EShop/Controllers/Client/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core6_EShop.Controllers.Client
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}

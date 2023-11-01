068c96fc3c762b648ab3f513003bcfa639fddd27
john <john@mail.aifu.com.tw>
2023-11-01 14:14:30 +0800

    新增伺服端

---
+++ Core6_EShop/Controllers/Server/ServerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core6_EShop.Controllers.Server
+{
+    public class ServerController : Controller
+    {
+        public IActionResult GoodsManage()
+        {
+            return View();
+        }
+    }
+}
